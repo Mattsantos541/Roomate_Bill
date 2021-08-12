a63e2a1a30431c6e8cea8491abcb5c09a9acbf16
Mattsantos541 <mattsantos541@gmail.com>
2021-08-12 16:33:11 -0500

    starting

---
+++ main.py
@@ -0,0 +1,32 @@
+class Bill:
+    """
+    Object contains data of bill such as total amount and period of bill
+    """
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+        
+class Flatmate:
+    """Create a flatmate person who lives in the
+    flat and pays a share of the bill"""
+        
+        
+    def __init__(self, name, days_in_the_house):
+        self.days_in_the_house = days_in_the_house
+        self.name = name
+
+    def pay(self, bill):
+        pass
+
+
+class PdfReport:
+    """Create a Pdf file that contains data about the flatmates:
+    their names, due amount, period of the bill"""
+
+    def __init__(self, filename):
+        self.filename = filename
+
+        def generate(self, flatmate1, flatmate2, bill):
+            pass
+
+
