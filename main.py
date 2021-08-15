483000ae8d267875b2077d5cd86f267d160e2a62
Mattsantos541 <mattsantos541@gmail.com>
2021-08-15 08:18:12 -0500

    update Roomate_bill

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ class Bill:
     """
     Object contains data of bill such as total amount and period of bill
     """
+
     def __init__(self, amount, period):
         self.amount = amount
         self.period = period
-        
+
+
 class Flatmate:
     """Create a flatmate person who lives in the
     flat and pays a share of the bill"""
-        
-        
+
     def __init__(self, name, days_in_the_house):
         self.days_in_the_house = days_in_the_house
         self.name = name
@@ -26,7 +27,5 @@ class PdfReport:
     def __init__(self, filename):
         self.filename = filename
 
-        def generate(self, flatmate1, flatmate2, bill):
-            pass
-
-
+    def generate( self, flatmate1, flatmate2, bill):
+        pass
