9b31d41643ca46d5b72a258c2e97d8ff2a1e9941
Mattsantos541 <mattsantos541@gmail.com>
2021-08-18 10:50:05 -0500

    corrections made after push error

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ class Bill:
     """
     Object contains data of bill such as total amount and period of bill
     """
+
     def __init__(self, amount, period):
         self.amount = amount
         self.period = period
-        
+
+
 class Flatmate:
     """Create a flatmate person who lives in the
     flat and pays a share of the bill"""
-        
-        
-    def __init__(self, name, days_in_the_house):
-        self.days_in_the_house = days_in_the_house
+
+    def __init__(self, name, days_in_house):
+        self.days_in_house = days_in_house
         self.name = name
 
-    def pay(self, bill):
-        pass
+    def pay(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
 
 
 class PdfReport:
@@ -29,4 +32,9 @@ class PdfReport:
         def generate(self, flatmate1, flatmate2, bill):
             pass
 
+the_bill = Bill(amount=120, period="March 2021")
+john = Flatmate(name="john", days_in_house=20)
+mary = Flatmate(name="mary", days_in_house=25)
 
+print("John pays: ", john.pay(bill=the_bill, flatmate2=mary))
+print("Mary pays: ", mary.pay(bill=the_bill, flatmate2=john))
