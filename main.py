81adeab1d2628eb3e0a5e558e1fa42926d891fce
Mattsantos541 <mattsantos541@gmail.com>
2021-08-21 11:28:40 -0500

    Before update nd restart of pycharm

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from FPDF import FPDF
+
 class Bill:
     """
     Object contains data of bill such as total amount and period of bill
@@ -30,11 +32,32 @@ class PdfReport:
         self.filename = filename
 
         def generate(self, flatmate1, flatmate2, bill):
-            pass
+            pdf = FPDF(orientation='P', unit='pt', format='A4')
+            pdf.add_page()
 
+            # Add the title
+            pdf.set_font(family='Times', size=24, style='C')
+            pdf.cell(w=0, h=80, txt="Roommate Bill", border=1, align="C", ln=1)
+
+            # Insert period label and value
+            pdf.cell(w=100, h=40, txt="Period:", border=1)
+            pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+
+            # Insert name and due amount of the first flatmate
+            pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
+            pdf.cell(w=150, h=40, txt=flatmate1.pay(), border=1, ln=1)
+
+            # Insert name and due amount of the second flatmate
+            pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
+            pdf.cell(w=150, h=40, txt=flatmate2.pay(), border=1, ln=1)
+
+            pdf.output("bill.pdf")
 the_bill = Bill(amount=120, period="March 2021")
 john = Flatmate(name="john", days_in_house=20)
 mary = Flatmate(name="mary", days_in_house=25)
 
 print("John pays: ", john.pay(bill=the_bill, flatmate2=mary))
 print("Mary pays: ", mary.pay(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename= "Report1.pdf")
+pdf_report.generate(flatmate1=john, flatmate2= mary, bill= the_bill)
